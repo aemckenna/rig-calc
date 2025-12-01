5c0e2bdf4c653879b7753ff71ec012517f07f46b
Asher McKenna <mr.ashermckenna@gmail.com>
2025-12-01 08:34:12 -0500

    DOM content load

--- universe-grid.js
+++ universe-grid.js
@@ -5,3 +5,11 @@ let universeGridContainer;
 let gridUniverseInput;
 let gridUniverseForm;
 let gridUniverseHint;
+
+document.addEventListener("DOMContentLoaded", () => {
+  universeGridContainer = document.getElementById("universeGrid");
+  gridUniverseInput = document.getElementById("gridUniverseInput");
+  gridUniverseForm = document.getElementById("gridUniverseForm");
+  gridUniverseHint = document.getElementById("gridUniverseHint");
+
+  loadRigFromStorage();
